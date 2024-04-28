b39996564029c8674db7f9b2f694f79d383ab38a
chirandck2 <chirandck2@gmail.com>
2024-04-28 16:33:30 +0530

    addeda

---
+++ worldcup.java
@@ -0,0 +1,26 @@
+package com.poornima;
+
+public class bank {
+
+	apublic static void atm() {
+		
+		aSystem.out.println("hi, welcome to the banking");
+                a 12345
+			 System.out.println(" Modified code");
+a System.out.println("From local to repository");
+		 System.out.println("From lrepo to local");
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		atm()1234;
+btm()1234;
+btm()1234;
+btm()1234;
+		
+	}
+	
+}
